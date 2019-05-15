f4e1968dc6d66504761679a8c6de88269d5bc856
Dimitrije M <dimitrije@kolosek.com>
2019-05-15 16:46:16 +0200

    [#165933855] Created model for tags

--- app/models/story.rb
+++ app/models/story.rb
@@ -2,4 +2,6 @@ class Story < ApplicationRecord
   belongs_to :owner, class_name: "User"
   has_many :story_editors
   has_many :users, through: :story_editors
+  has_many :taggings
+  has_many :tags, through: :taggings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_15_111335) do
+ActiveRecord::Schema.define(version: 2019_05_15_142133) do
 
   create_table "stories", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,22 @@ ActiveRecord::Schema.define(version: 2019_05_15_111335) do
     t.datetime "updated_at", null: false
     t.index ["story_id"], name: "index_story_editors_on_story_id"
     t.index ["user_id"], name: "index_story_editors_on_user_id"
+  end
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer "tag_id"
+    t.integer "story_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["story_id"], name: "index_taggings_on_story_id"
+    t.index ["tag_id"], name: "index_taggings_on_tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
   create_table "users", force: :cascade do |t|
