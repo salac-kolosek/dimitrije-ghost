bccfce0f677dc3e5bb0444dbfd8d086db6238dd0
Dimitrije M <dimitrije@kolosek.com>
2019-05-16 11:48:35 +0200

    [#165933858] Autocomplete for search

---
+++ app/models/search_suggestion.rb
@@ -0,0 +1,20 @@
+class SearchSuggestion < ApplicationRecord
+  
+  def self.terms_for(prefix)
+    suggestions = where("term like ?", "#{prefix}_%")
+    suggestions.order("popularity desc").limit(10).pluck(:term)
+  end
+
+  def self.index_products
+    Story.find_each do |story|
+      index_term(story.title)
+      story.title.split.each { |t| index_term(t) }
+    end
+  end
+
+  def self.index_term(term)
+    where(term: term.downcase).first_or_create.tap do |suggestion|
+      suggestion.increment! :popularity
+    end
+  end
+end
